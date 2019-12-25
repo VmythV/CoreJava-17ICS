17391643c93c397d557f3327661bd0a3e5aeaa98
VmythV <37138541+VmythV@users.noreply.github.com>
2019-12-26 00:32:59 +0800

    更新Week08

---
+++ Week08/src/codes_my/Problem_A.java
@@ -0,0 +1,24 @@
+package codes_my;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Problem_A {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n  =cin.nextInt();
+        int[] arr = new int[n];
+        Set<Integer> set = new TreeSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = cin.nextInt();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+        for (Integer a:set ) {
+            System.out.println(a);
+        }
+
+    }
+}
